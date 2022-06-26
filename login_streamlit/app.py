6c43743e65732ad82e547bb0e70e385ae467a8bd
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-06-25 23:36:47 -0700

    first section done

--- login_streamlit/app.py
+++ login_streamlit/app.py
@@ -4,7 +4,7 @@ def main():
 
     st.title('Test Vision')
     menu = ['Home', 'Login', 'SignUp']
-    choice = st.sidebar.selectbox('Menu')
+    choice = st.sidebar.selectbox('Menu', menu)
 
 
     if choice == 'Home':
@@ -12,8 +12,29 @@ def main():
 
     elif choice == 'Login':
         st.subheader('Login Section')
+        username = st.sidebar.text_input('User Name')
+        password = st.sidebar.text_input('Password', type='password')
+
+        if password == '12345':
+            if st.sidebar.checkbox('Login'):
+                st.success('Logged in as {}'.format(username))
+                task = st.selectbox('Task', ['Test checker', 'Statistics'])
+                if task == 'Test checker':
+                    st.subheader('Test checking')
+                elif task == 'Statistics':
+                    st.subheader('Informations')
+        else:
+            st.warning("Incorrect Username or Password")
+
 
     elif choice == 'SignUp':
         st.subheader('Create New Account')
+        new_user = st.text_input('Username')
+        new_password = st.text_input('Password', type='password')
+
+        if st.button('SignUp'):
+            st.success('You have successfully created an account')
+            st.info('Go to login menu to login')
+
 if __name__ == '__main__':
     main()
